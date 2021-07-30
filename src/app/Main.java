8b3e554098805a5a0f8841dba473fd0ccc378373
VinyMT <viniciusmatiolatramontin@gmail.com>
2021-07-30 11:16:11 -0300

    Created chess layer classes

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,11 +1,13 @@
 package app;
 
+import chess.ChessMatch;
 import tabuleiro.Position;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		ChessMatch match = new ChessMatch();
+		UI.printBoard(match.getPieces());
 	}
 
 }
